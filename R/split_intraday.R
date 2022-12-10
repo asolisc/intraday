d62d099dcd5b550eecab05b582df235294ed119c
asolisc <alexis.solisc@gmail.com>
2022-12-09 22:19:36 -0600

    Modifying split_intraday function.

--- R/split_intraday.R
+++ R/split_intraday.R
@@ -1,12 +1,12 @@
 split_intraday <- function(data, 
-                           grid = 1, 
-                           .date_col = index_date) {
+                           grid = 5, 
+                           date_col = index_date) {
   
   data %>%
-    dtplyr::lazy_dt() %>% 
+    # dtplyr::lazy_dt() %>% 
     
     # We group by index_date since all the operations are done per day
-    group_by({{.date_col}}) %>% 
+    group_by({{date_col}}) %>% 
     
     # Get and ID for each trade
     mutate(id = row_number() - 1, .after = 3) %>% 
@@ -14,13 +14,14 @@ split_intraday <- function(data,
     summarise(
       index_time,
       sparse_return = log(last_price/lag(last_price)),
-      .groups = 'drop') %>% 
+      .groups = 'drop'
+      ) %>% 
     
     mutate(sparse_grid = grid) %>% 
     
     # Join with the price at each minute
     left_join(data %>% 
-                select({{.date_col}}, index_time, last_price),
+                select({{date_col}}, index_time, last_price),
               by = c("index_date", "index_time")) %>% 
     as_tibble()
 }
